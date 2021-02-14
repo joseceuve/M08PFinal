0fcc4403e084e7cf1cc9a627451f635777ad468b
joseceuve <71776803+joseceuve@users.noreply.github.com>
2021-02-14 09:55:33 +0100

    Inicio

---
+++ app/src/main/java/cat/copernic/pfinal/jose/m08pfinal/Fragment_Detalle.kt
@@ -0,0 +1,29 @@
+package cat.copernic.pfinal.jose.m08pfinal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Fragmento que muestra el detalle
+ */
+class Fragment_Detalle : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__detalle, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/cat/copernic/pfinal/jose/m08pfinal/Fragment_Vistas.kt
@@ -0,0 +1,26 @@
+package cat.copernic.pfinal.jose.m08pfinal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Fragmento de las vistas
+ */
+class Fragment_Vistas : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__vistas, container, false)
+    }
+
+}
